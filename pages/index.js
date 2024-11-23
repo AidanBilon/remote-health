2ce0565ea68f4e52f3803a9e16db2eba5dee44a3
JustaShadow2 <JustaShadow2>
2024-11-23 18:04:09 -0500

    adding login page

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,59 @@ export default function Broadcast() {
   const title = `Amazon IVS – Web Broadcast Tool`;
   return (
     <>
-      <Head>
+
+    <div className="bg-gray-100 flex items-center justify-center min-h-screen">
+      <div className="w-full max-w-sm bg-white rounded-lg shadow-md p-6">
+        <h2 className="text-2xl font-bold text-center text-gray-800 mb-4">Login</h2>
+        <form>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="mt-1 block w-full px-4 py-2 border rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500 border-gray-300"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="mt-1 block w-full px-4 py-2 border rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500 border-gray-300"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+
+          <Link href="/dashboard">Login</Link>
+          
+
+          
+        </form>
+
+        
+        <p className="text-sm text-center text-gray-600 mt-4">
+          Don't have an account!{' '}
+          <a href="#" className="text-blue-500 hover:underline">
+            Sign Up
+          </a>
+        </p>
+      </div>
+    </div>
+    </>
+  );
+}
+
+/*
+
+<Head>
         <title>{title}</title>
         <meta
           name='description'
@@ -36,6 +88,8 @@ export default function Broadcast() {
           </LocalMediaProvider>
         </UserSettingsProvider>
       </ModalProvider>
-    </>
-  );
-}
+
+
+
+
+*/
